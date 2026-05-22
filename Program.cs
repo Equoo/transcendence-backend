79c1e2c10dc18b1f1c27f4689c8a644e5899c232
Pierre Jarnac <pjarnac@student.42lyon.fr>
2026-05-22 16:41:38 +0000

    feat: allow all origins for CORS

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin();
+        builder.AllowAnyHeader();
+        builder.AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors();
+
+app.MapGet("/", () => "Hello World from API!");
 
 app.Run();
