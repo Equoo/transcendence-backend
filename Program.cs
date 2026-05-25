e3ceccc1012d6f875ff5995a5cbfd4b4c138649f
Pierre Jarnac <pjarnac@student.42lyon.fr>
2026-05-25 17:57:13 +0000

    fix: since we use proxied api call we do not need CORS

--- Program.cs
+++ Program.cs
@@ -1,18 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(builder =>
-    {
-        builder.AllowAnyOrigin();
-        builder.AllowAnyHeader();
-        builder.AllowAnyMethod();
-    });
-});
-
 var app = builder.Build();
-
-app.UseCors();
 
 app.MapGet("/", () => "Hello World from API!");
 app.MapGet("/api", () => "Hello World from API Path!");
