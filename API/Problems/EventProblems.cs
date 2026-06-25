12fc0f38408d8a221954546422076158d3269d6b
Pierre Jarnac <pjarnac@student.42lyon.fr>
2026-06-25 16:12:38 +0000

    feat: implement ProblemDetails responses for specific endpoints errors

---
+++ API/Problems/EventProblems.cs
@@ -0,0 +1,32 @@
+namespace KeepGrouped.API.Problems;
+
+static public class EventProblems
+{
+    static public IResult AlreadyRegistered(string username, string eventName)
+    {
+        return Results.Problem(
+            title: "Already Registered",
+            detail: $"User '{username}' is already registered to '{eventName}'.",
+            statusCode: StatusCodes.Status409Conflict,
+            extensions: new Dictionary<string, object?> { ["errorCode"] = "ALREADY_REGISTERED" }
+        );
+    }
+    static public IResult NotRegistered(string username, string eventName)
+    {
+        return Results.Problem(
+            title: "Not Registered",
+            detail: $"User '{username}' is not registered to '{eventName}'.",
+            statusCode: StatusCodes.Status404NotFound,
+            extensions: new Dictionary<string, object?> { ["errorCode"] = "NOT_REGISTERED" }
+        );
+    }
+    static public IResult EventFull()
+    {
+        return Results.Problem(
+            title: "Event is full",
+            detail: "The event has already reach the full capacity.",
+            statusCode: StatusCodes.Status409Conflict,
+            extensions: new Dictionary<string, object?> { ["errorCode"] = "EVENT_FULL" }
+        );
+    }
+}
