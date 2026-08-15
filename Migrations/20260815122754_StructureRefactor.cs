ed60997af19799ef7861baeabf939b96701ff25d
Pierre Jarnac <pjarnac@student.42lyon.fr>
2026-08-15 12:28:07 +0000

    fix: add missing migration

---
+++ Migrations/20260815122754_StructureRefactor.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KeepGrouped.Migrations
+{
+    /// <inheritdoc />
+    public partial class StructureRefactor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Registration_EventRoles_RoleId",
+                table: "Registration");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RoleId",
+                table: "Registration",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Registration_EventRoles_RoleId",
+                table: "Registration",
+                column: "RoleId",
+                principalTable: "EventRoles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Registration_EventRoles_RoleId",
+                table: "Registration");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RoleId",
+                table: "Registration",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Registration_EventRoles_RoleId",
+                table: "Registration",
+                column: "RoleId",
+                principalTable: "EventRoles",
+                principalColumn: "Id");
+        }
+    }
+}
